89d13b5e74d1bc5ba4b57ccc1a0dd8cef66904d5
Cayden Anthony <ca499921@gmail.com>
2023-01-24 20:02:49 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Cayden Anthony>
+ *        Date: <jan 24 23>
+ * Description: lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
